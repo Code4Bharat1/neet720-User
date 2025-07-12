158ac845c26e4be7f11f7fe8f518c10d6b1cb147
AtulmishraIT <155865013+AtulmishraIT@users.noreply.github.com>
2025-07-12 15:38:13 +0530

    main: Fixed

---
+++ src/app/landing/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateTestPage from '@/components/landingPage/createtest';
+import FastQuizFeature from '@/components/landingPage/fastquiz';
+
+export default function page() {
+  return (
+    <div>
+      <CreateTestPage />
+      <FastQuizFeature />
+    </div>
+  )
+}
