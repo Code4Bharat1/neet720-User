137a78d1a61fa8321e3912552c55bc69056e9879
monis-70 <mansurimonis8@gmail.com>
2025-07-19 19:17:13 +0530

    changes in layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import ClientLayout from "./clientlayout";
+import ClientLayout from "./clientlayout.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
