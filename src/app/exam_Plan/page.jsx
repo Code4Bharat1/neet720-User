f1362d50a2e1b4af324605b7b36a913a95a7929a
yogu-code <yogeshrane019@gmail.com>
2025-11-22 17:28:48 +0530

    update page.jsx

--- src/app/exam_Plan/page.jsx
+++ src/app/exam_Plan/page.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   description:
     "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
   openGraph: {
-    url: "https://neet720.com/exam_plan",
+    url: "https://neet720.com/exam_Plan",
     title: "exam_Plan | NEET720",
     description:
       "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
