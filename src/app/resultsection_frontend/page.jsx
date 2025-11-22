ce28d223779b548faa8b7de8214227b733940b7d
abdulahadkhan-10 <st.abdul24@gmail.com>
2025-11-23 04:11:18 +0530

    update page.jsx

--- src/app/resultsection_frontend/page.jsx
+++ src/app/resultsection_frontend/page.jsx
@@ -1,31 +1,31 @@
-export const metadata = {
-  title:
-    "resultSection_frontend | NEET720 – India's Most Trusted NEET Preparation Platform",
-  description:
-    "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
-  openGraph: {
-    url: "https://neet720.com/resultsection_frontend",
-    title: "resultSection_frontend | NEET720",
-    description:
-      "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
-    siteName: "NEET720",
-  },
-};
+// export const metadata = {
+//   title:
+//     "resultSection_frontend | NEET720 – India's Most Trusted NEET Preparation Platform",
+//   description:
+//     "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
+//   openGraph: {
+//     url: "https://neet720.com/resultsection_frontend",
+//     title: "resultSection_frontend | NEET720",
+//     description:
+//       "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
+//     siteName: "NEET720",
+//   },
+// };
 
-import FeatureResult from '@/components/resultSection_frontend/featureResult'
-import ResultSection from '@/components/resultSection_frontend/resultSection'
-import React from 'react'
+// import FeatureResult from '@/components/resultSection_frontend/featureResult'
+// import ResultSection from '@/components/resultSection_frontend/resultSection'
+// import React from 'react'
 
-const Page = () => {
-  return (
-    <div>
-     <ResultSection/>
-      <div className="min-h-screen bg-[#103f5d] flex items-center justify-center py-20">
+// const Page = () => {
+//   return (
+//     <div>
+//      <ResultSection/>
+//       <div className="min-h-screen bg-[#103f5d] flex items-center justify-center py-20">
      
-        <FeatureResult/>
-      </div>
-    </div>
-  )
-}
+//         <FeatureResult/>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Page
+// export default Page
