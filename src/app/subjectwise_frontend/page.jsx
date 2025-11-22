d40ca1cde55a28ff1895d0f9c3a85d620cd84b5b
abdulahadkhan-10 <st.abdul24@gmail.com>
2025-11-23 03:37:11 +0530

    chore: move page.jsx to src/app/subjectwise_frontend

--- src/app/subjectwise_frontend/page.jsx
+++ src/app/subjectwise_frontend/page.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   description:
     "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
   openGraph: {
-    url: "https://neet720.com/Subjectwise_frontend",
+    url: "https://neet720.com/subjectwise_frontend",
     title: "Subjectwise_frontend | NEET720",
     description:
       "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
@@ -14,15 +14,16 @@ export const metadata = {
 
 import FeatureResult from '@/components/resultSection_frontend/featureResult'
 import ResultSection from '@/components/resultSection_frontend/resultSection'
-import SubjectWise from '@/components/Subjectwise_frontend/Subjectwise'
-import FeatureSub from '@/components/Subjectwise_frontend/FeatureSub'
+import FeatureSub from '@/components/Subjectwise_frontend/FeatureSub';
+import SubjectWise from '@/components/Subjectwise_frontend/Subjectwise';
+
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
      <SubjectWise/>
-      <div className="min-h-screen bg-[#103f5d] flex items-center justify-center py-20">
+           <div className="min-h-screen bg-[#103f5d] flex items-center justify-center py-20">
      
         <FeatureSub/>
       </div>
