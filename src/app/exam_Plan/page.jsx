5792802d93f8ae57d9e6dfe42cf7bc42f2861566
yogu-code <yogeshrane019@gmail.com>
2025-11-22 17:14:36 +0530

    update page.jsx

--- src/app/exam_Plan/page.jsx
+++ src/app/exam_Plan/page.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   description:
     "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
   openGraph: {
-    url: "https://neet720.com/exam_Plan",
+    url: "https://neet720.com/exam_plan",
     title: "exam_Plan | NEET720",
     description:
       "NEET720 offers India’s best NEET mock tests, PYQs, analytics, AIR prediction, and smart learning tools for NEET aspirants.",
@@ -12,7 +12,7 @@ export const metadata = {
   },
 };
 
-import ExamPlan from '@/components/exam_Plan/ExamPlan.jsx';
+import ExamPlan from '@/components/exam_plan/ExamPlan';
 import React from 'react'
 
 const Page = () => {
