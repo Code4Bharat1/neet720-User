6a67a6cb665b833dde3bf0b8527f2e887b6a3ff7
yogu-code <yogeshrane019@gmail.com>
2025-11-22 16:49:23 +0530

    test: update page.jsx

--- src/app/LP_createtest/page.jsx
+++ src/app/LP_createtest/page.jsx
@@ -1,4 +1,3 @@
-import CreateTestPage from "@/components/LP_createtest/createtest";
 
 export const metadata = {
   title:
@@ -15,24 +14,6 @@ export const metadata = {
 };
 
 import CreateTestPage from "@/components/LP_createtest/createtest";
-
-
-// const CreateTestPage = () => (
-//   <div>
-//     <h2>Create Test Page</h2>
-//     {/* Add your test creation UI here */}
-//   </div>
-// );
-
-// export default function page() {
-//   return (
-//     <div>
-//       <CreateTestPage />
-//     </div>
-//   );
-// }
-
-
 
 export default function page() {
   return (
